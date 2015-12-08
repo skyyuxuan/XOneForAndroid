ecf0ef32edca50d3d56e32e7432c1faffe500a98
unknown <j-xuanyu@SkyXuan.chn.gbl>
2015-12-08 17:23:35 +0800

    Initial commit

---
+++ app/src/main/java/datahelper/network/IHttpRequest.java
@@ -0,0 +1,17 @@
+package datahelper.network;
+
+
+import java.util.Map;
+
+/**
+ * Created by j-xuanyu on 2015/12/8.
+ */
+public interface IHttpRequest {
+    String Url = null;
+    String Method = null;
+    String ContentType = null;
+    long ContentRange = 0;
+    String Accept = null;
+    Map<String, String> Headers = null;
+    String Body = null;
+}
