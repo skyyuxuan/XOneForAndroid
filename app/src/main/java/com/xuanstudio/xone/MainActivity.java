4d3af50ed2dff7d9f60c82d7080fb093e47fb70e
xuanyu <yuxuansha@qq.com>
2015-12-08 17:36:50 +0800

    first commit

--- app/src/main/java/com/xuanstudio/xone/MainActivity.java
+++ app/src/main/java/com/xuanstudio/xone/MainActivity.java
@@ -7,6 +7,8 @@ import android.widget.TextView;
 public class MainActivity extends AppCompatActivity {
 
     TextView tv;
+    TextView tv2;
+    TextView tv3;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
